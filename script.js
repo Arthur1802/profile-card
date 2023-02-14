7bcf3dddc785ba9bb85b45ef8d474e0cab623087
Arthur1802 <arthur.barreto@soulasalle.com.br>
2023-02-13 22:06:40 -0300

    Files uploaded

---
+++ script.js
@@ -0,0 +1,17 @@
+var logos = document.querySelectorAll("i")
+var titles = document.querySelectorAll("h3")
+var text = document.querySelectorAll(".text")
+
+function hoverOn(i)
+{
+    logos[i].style.left = '35%'
+    titles[i].style.left = '36.5%'
+    titles[i].style.opacity = '1'
+}
+
+function hoverOff(i)
+{
+    titles[i].style.opacity = '0'
+    titles[i].style.left = '35%'
+    logos[i].style.left = '45%'
+}
